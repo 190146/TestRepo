7cf23f80b40fe2caa431721ef689ec42ee7e6c95
190714 <102937700+190714@users.noreply.github.com>
2022-06-03 15:35:58 +0900

    kadai9

---
+++ MyBootApp9/src/main/java/jp/te4a/spring/boot/myapp9/BookBean.java
@@ -0,0 +1,32 @@
+package jp.te4a.spring.boot.myapp9;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "books")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookBean {
+	@Id
+	@GeneratedValue
+	private Integer id;
+	@Column(nullable = false)
+	private String title;
+	private String writter;
+	private String publisher;
+	private Integer price;
+	
+	//以下@Dataにより省略
+	/*
+	public Integer getId() {
+		// TODO Auto-generated method stub
+		return null;
+	//BookRepositoryのgetid()の部分
+	}
+	 */
+	
+}
